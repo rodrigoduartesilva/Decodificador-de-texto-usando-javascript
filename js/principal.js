ed8189d9e7b0c681e0814ec67f2692cab7fed89a
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-06-15 17:13:32 -0300

    Novo Commit

--- js/principal.js
+++ js/principal.js
@@ -5,8 +5,6 @@ var containerSaida = document.querySelector(".container-saida");
 var containerSaidaResultado = document.querySelector(".container-saida-resultado");
 var letrasAcentuadas = /[áàâãéèêíïóôõöúçñ]+/i;
 
-/*Evento de Click para os botões de Criptografar, Descriptografar e Copiar*/
-
 btnCripto.addEventListener("click", function(){
     var textoDigitado = document.querySelector("#entrada-cripto").value;
     var paragrafo = document.createElement("p");
@@ -43,48 +41,3 @@ btnCopiar.addEventListener("click", function(){
     var campoResultado = document.querySelector(".container-saida-resultado").textContent;
     navigator.clipboard.writeText(campoResultado);
 });
-
-
-/*------------------------------------------------------------------------*/
-
-function criptografarTextoDigitado(texto){
-    const criptografiaTexto = ["enter", "imes", "ai", "ober", "ufat"];
-    const descriptografiaTexto = ["e", "i", "a", "o", "u"];
-
-     var resultadoCripto = texto.replace(/e|i|a|o|u/gi, function(texto){
-        if(texto == descriptografiaTexto[0]){
-            return criptografiaTexto[0];
-        } else if(texto == descriptografiaTexto[1]){
-            return criptografiaTexto[1];
-        } else if(texto == descriptografiaTexto[2]){
-            return criptografiaTexto[2];
-        } else if(texto == descriptografiaTexto[3]){
-            return criptografiaTexto[3];
-        } else if(texto == descriptografiaTexto[4]){
-            return criptografiaTexto[4];
-        }
-    });
-
-    return resultadoCripto;
-}
-
-function DescriptocriptografarTextoDigitado(texto){
-    const criptografiaTexto = ["enter", "imes", "ai", "ober", "ufat"];
-    const descriptografiaTexto = ["e", "i", "a", "o", "u"];
-
-     var resultadoDescripto = texto.replace(/enter|imes|ai|ober|ufat/gi, function(texto){
-        if(texto == criptografiaTexto[0]){
-            return descriptografiaTexto[0];
-        } else if(texto == criptografiaTexto[1]){
-            return descriptografiaTexto[1];
-        } else if(texto == criptografiaTexto[2]){
-            return descriptografiaTexto[2];
-        } else if(texto == criptografiaTexto[3]){
-            return descriptografiaTexto[3];
-        } else if(texto == criptografiaTexto[4]){
-            return descriptografiaTexto[4];
-        }
-    });
-
-    return resultadoDescripto;
-}
